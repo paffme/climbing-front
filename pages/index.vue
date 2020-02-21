7d28532b68d1c96f0cabf04c3d1f23a7bf5605a9
Ruben DESERT <ruben.desert@eemi.com>
2020-02-21 07:36:18 +0100

    first init

---
+++ pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="Free"
+        icon="github-circle"
+      >
+        Open source on <a href="https://github.com/buefy/buefy">
+          GitHub
+        </a>
+      </card>
+
+      <card
+        title="Responsive"
+        icon="cellphone-link"
+      >
+        <b class="has-text-grey">
+          Every
+        </b> component is responsive
+      </card>
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        No other internal dependency
+      </card>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Card from '~/components/Card.vue'
+import { Vue, Component } from 'vue-property-decorator'
+
+Component.registerHooks([
+  'beforeRouteEnter',
+  'beforeRouteLeave',
+  'beforeRouteUpdate'
+])
+
+@Component({
+  components: { Card }
+})
+export default class HomePage extends Vue {
+  // Hooks
+  beforeRouteEnter(to: any, from: any, next: any) {
+    console.log('beforeRouteEnter')
+    next()
+  }
+
+  beforeRouteUpdate(to: any, from: any, next: any) {
+    console.log('beforeRouteUpdate')
+    next()
+  }
+
+  beforeRouteLeave(to: any, from: any, next: any) {
+    console.log('beforeRouteLeave')
+    next()
+  }
+  // Properties
+
+  // Component Data
+  message: string = 'This is a message'
+
+  // Computed properties
+
+}
+</script>
---
+++ pages/user/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <h1>User Id Page</h1>
+  </section>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from "vue-property-decorator";
+
+  @Component
+    export default class UserId extends Vue {
+  }
+</script>
+
+<style scoped>
+
+</style>
