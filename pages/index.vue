5229e7daa147794e00744d40baf465f0447897f6
Ruben DESERT <ruben.desert@eemi.com>
2020-03-26 09:33:32 +0100

    test deploy master

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section">
-    <h1>Test Deploy - 1</h1>
+    <h1>Test Deploy - 3</h1>
     <div class="columns is-mobile">
       <card title="Free" icon="github-circle">
         Open source on
