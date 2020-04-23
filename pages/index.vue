dd7d0f8ba7893c940fbb2102de47dfddbbff7c33
Ruben DESERT <ruben.desert@eemi.com>
2020-04-23 13:30:09 +0200

    Add Cookie & authUser

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Dashboard</h1>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { authUser } from "~/store";
+
+@Component({
+  middleware: 'isAuth'
+})
+export default class HomePage extends Vue {
+  // Properties
+
+  // Component Data
+  message: string = 'This is a message'
+
+  // Computed properties
+}
+</script>
