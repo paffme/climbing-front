0a22ec0f27de818e6bd67d9efa63338e08d2f9ea
DamSaulGoodMan <capron.damien@laposte.net>
2020-05-26 19:21:25 +0200

    try to generify datepicker auto selection...

--- cypress/integration/competitions/creation_spec.ts
+++ cypress/integration/competitions/creation_spec.ts
@@ -36,32 +36,37 @@ describe('Test for Creation of Competition', () => {
     cy.wait(200)
   })
 
-/*  it('Must enter the create competition view', () => {
+  it('Must enter the create competition view', () => {
     cy.visit("")
 
     cy.get(".page_header > div > .button > :nth-child(1)").click()
 
     cy.url().should('equal',
       Cypress.config().baseUrl + 'competitions/create')
-  })*/
-
+  })
+  
   it('Must create a competition', () => {
     cy.visit('competitions/create')
-    cy.url().should('equal',
-      Cypress.config().baseUrl + 'competitions/create')
 
-    const compt: Competition = new Competition()
+    // cy.get('.date-welcome').click()
+    // cy.get('.dropdown-menu').get('.pagination-list > .has-addons')
+    cy.typeDate('.date-welcome', new Date())
 
-    cy.get(".name").type("{ctrl}a").type(compt.name)
-    cy.typeDate(".date-start", compt.startDate)
-    cy.typeDate(".date-end", compt.endDate)
-    cy.get(".status").click()
-    cy.typeDate(".date-welcome", compt.welcomeDate)
-    cy.get(".street").type("{ctrl}a").type(compt.street)
-    cy.get(".city").type("{ctrl}a").type(compt.city)
-    cy.get(".description").type("{ctrl}a").type(compt.description)
-    cy.get(".agenda").type("{ctrl}a").type(compt.agenda)
-
-    cy.get(".create-competition").click()
+    // cy.url().should('equal',
+    //   Cypress.config().baseUrl + 'competitions/create')
+    //
+    // const compt: Competition = new Competition()
+    //
+    cy.get(".name").type("{ctrl}a").type("toto")
+    // cy.typeDate(".date-start", compt.startDate)
+    // cy.typeDate(".date-end", compt.endDate)
+    // cy.get(".status").click()
+    // cy.typeDate(".date-welcome", compt.welcomeDate)
+    // cy.get(".street").type("{ctrl}a").type(compt.street)
+    // cy.get(".city").type("{ctrl}a").type(compt.city)
+    // cy.get(".description").type("{ctrl}a").type(compt.description)
+    // cy.get(".agenda").type("{ctrl}a").type(compt.agenda)
+    //
+    // cy.get(".create-competition").click()
   })
 })
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -18,19 +18,32 @@ function login(email: string, password: string): Cypress.Chainable {
 }
 Cypress.Commands.add("login", login)
 
+  // .field-body > :nth-child(1) > .datepicker > .dropdown > .dropdown-menu > .dropdown-content > .dropdown-item > :nth-child(1) > :nth-child(1) > .pagination > .pagination-list > .field > :nth-child(1) > .select > select
+
 function typeDate(datePickerRef: string, date: Date): Cypress.Chainable {
   cy.get(datePickerRef).click()
-  cy.get('.pagination-list > .has-addons')
-    .get('select').each(($child, index) => {
-      if(index == 0) {
-        cy.wrap($child).select(String(date.getMonth() - 1));
-      }
-      else if (index == 1) {
-        cy.wrap($child).select(String(date.getFullYear()));
-      }
-    })
-
-  return cy.get('.datepicker-body > .datepicker-row > .is-selectable')
-    .contains(String(date.getDate())).click()
+    .get(".datepicker:visible > .dropdown >" +
+      " .dropdown-menu > .dropdown-content > .dropdown-item > :nth-child(1) >" +
+      " :nth-child(1) > .pagination > .pagination-list > .field >" +
+      " :nth-child(1) > .select > select")
+    .select(String(date.getMonth() - 1))
+  cy.get(".datepicker:visible > .dropdown >" +
+    " .dropdown-menu > .dropdown-content > .dropdown-item > :nth-child(1) >" +
+    " :nth-child(1) > .pagination > .pagination-list > .field >" +
+    " :nth-child(2) > .select > select")
+    .select(String(date.getFullYear()));
+  //   // .get('.field-body > :nth-child(1) > .dropdown-menu > .dropdown-content > .dropdown-item > :nth-child(1) > :nth-child(1) > .pagination > .pagination-list > .field')
+  //   .each(($child, index) => {
+  //   if(index == 0) {
+  //     cy.wrap($child).get('.select > select').select();
+  //   }
+  //   else if (index == 1) {
+  //     cy.wrap($child).select(String(date.getFullYear()));
+  //   }
+  // })
+  return cy
+  //
+  // return cy.get('.datepicker-body > .datepicker-row > .is-selectable')
+  //   .contains(String(date.getDate())).click()
 }
 Cypress.Commands.add("typeDate", typeDate)
