02817fd9a1453549ee4f260517037c79c955d270
DamSaulGoodMan <capron.damien@laposte.net>
2020-07-02 16:49:10 +0200

    add update button (not realy working)

---
+++ components/Button/ButtonSelectLocalFile.vue
@@ -0,0 +1,53 @@
+<template>
+	<section>
+		<b-field>
+			<b-upload v-model="fileDrop" drag-drop>
+				<section class="section">
+					<div class="content has-text-centered">
+						<p>
+							<b-icon icon="upload" size="is-large"></b-icon>
+						</p>
+						<p>Glissé une image ici ou appuyé pour ajouter une image</p>
+					</div>
+				</section>
+			</b-upload>
+		</b-field>
+		<div class="tags">
+			<span  v-for="(file, index) in fileDrop" class="tag is-primary" :key="index" >
+				{{file.name}}
+				<button class="delete is-small" type="button" @click="deleteFile()">
+				</button>
+			</span>
+		</div>
+	</section>
+</template>
+<!--<div v-if="!fileSelected" class="button-select-file">
+	<b-button :size="size" @click="">
+		Ajouter
+	</b-button>
+</div>
+<div v-else class="button-select-file">
+	<b-button :size="size" >
+		Changer
+	</b-button>
+</div>-->
+
+<script lang="ts">
+  import { Vue, Component, Prop } from 'vue-property-decorator'
+
+
+  @Component
+  export default class ButtonSelectLocalFile extends Vue {
+    @Prop(Object) fileSelected !: File | null
+
+    fileDrop: File[] = this.fileSelected ? new Array(this.fileSelected) : []
+
+    public deleteFile() {
+      this.fileDrop = [];
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/test/damien.vue
+++ pages/test/damien.vue
@@ -1,16 +1,20 @@
 <template>
-  <BouldersPicture
+  <button-select-local-file :fileSelected="null">
+
+  </button-select-local-file>
+<!--  <BouldersPicture
     :boulder-id="0"
     :competition="{ id: 0, name: 'Competition Damien' }"
     :round="{ id: 0, boulderIds: [0, 1, 2, 3, 4] }"
-  />
+  />-->
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import BouldersPicture from '~/components/BouldersSettingsComponent/BouldersPicture.vue'
+import ButtonSelectLocalFile from '~/components/Button/ButtonSelectLocalFile.vue'
 
-@Component({ components: { BouldersPicture } })
+@Component({ components: { BouldersPicture, ButtonSelectLocalFile} })
 export default class damien extends Vue {}
 </script>
 
