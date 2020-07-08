45092de3edabfd88fb9b9cde79d84e10fe41934d
DamSaulGoodMan <capron.damien@laposte.net>
2020-07-09 00:49:22 +0200

    try to annotate img BUT when refresh page, the img disappear. It is fun ! Fucking web.

---
+++ pages/test/annotation.vue
@@ -0,0 +1,34 @@
+<template>
+	<v-annotator :drawing=true
+	             :grid="[5, 5]"
+	             :height="img.height"
+	             :minSize="[50, 50]"
+	             :noInteract=true
+	             :noSelect=true
+	             :width="img.width"
+	             inertia
+	>
+		<img :src="require('~/assets/ffme_logo.png')" draggable="false" id="imgBlock"/>
+		<rect slot="drawing" stroke="red"/>
+
+	</v-annotator>
+</template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from "vue-property-decorator";
+  import VAnnotator from "vue-annotator";
+
+  @Component({ components: { VAnnotator } })
+  export default class annotation extends Vue {
+    img = new Image();
+
+    created() {
+      this.img.src = require("~/assets/ffme_logo.png");
+    }
+  }
+</script>
+
+<style scoped>
+	/*@import '~vue-annotator/style.css';*/
+
+</style>
